7cb1d70c8c19ed6f6793e8d8c16f58d6d90be03f
Lucas Oliveira <lcastro.oliveira@gmail.com>
2017-04-27 12:07:41 -0300

    Finish ch10, practical game setup done and initial specs and features written

---
+++ forca/features/step_definitions/game_steps.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+Quando(/^começo um novo jogo$/) do
+  run_interactive('forca')
+end
+
+Então(/^vejo na tela:$/) do |text|
+  assert_partial_output(text, all_stdout)
+end
---
+++ forca/lib/game.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+class Game
+
+  def initialize(output = STDOUT)
+    @output = output
+  end
+
+  def start
+    initial_message = 'Bem-vindo ao jogo da forca!'
+    @output.puts initial_message
+  end
+end
